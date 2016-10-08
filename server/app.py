b095572cbf4c8f0e88fb09b0b5fe831f3ed08a29
dridk <sacha@labsquare.org>
2016-10-08 12:34:26 +0200

    create server API REST

---
+++ server/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+import mongoengine as mongo 
+from models import Note
+
+mongo.connect("tododb")
+app = Flask(__name__)
+
+
+@app.route("/todo",methods = ['GET','POST'])
+def notes():
+	print (request.method)
+	if request.method == 'GET':
+		result = [note.export_data() for note in Note.objects.all()]
+		return jsonify({"success":True, "results": result}) 
+
+	if request.method == 'POST':
+		
+		try:
+			note = Note()
+			note.import_data(request.json)
+			note.save()
+		except:
+			return jsonify({"success":False, "message":"cannot save note"})
+		
+		return jsonify({"success":True, "results": str(note.id)})
+
+
+@app.route("/todo/<object_id>")
+def get_note(object_id):
+	try : 
+		note = Note.objects.get(pk = object_id)
+	except : 
+		return jsonify({"success":False, "message": "cannot find the note"})
+
+	return jsonify(note.export_data())
+
+@app.route("/todo/<object_id>", methods=["DELETE"])
+def delete_note(object_id):
+	try : 
+		note = Note.objects.get(pk = object_id)
+	except : 
+		return jsonify({"success":False, "message":"cannot find the note"})
+
+	note.delete()
+	return jsonify({"success":"true"})
+
+
+
+
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ server/models.py
@@ -0,0 +1,17 @@
+from mongoengine import * 
+
+class Note(Document):
+	title   = StringField(required=True, max_lengh=255)
+	checked = BooleanField(required=True, default=False)
+
+	def export_data(self):
+		return {
+		"id": str(self.id),
+		"title" : self.title,
+		"checked" : self.checked
+		}
+
+	def import_data(self,data):
+
+		self.title   = data.get("title")
+		self.checked = bool(data.get("checked",False))
