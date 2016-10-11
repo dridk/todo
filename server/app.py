6607ac028b1625de3583ea15c4b804bfb4ce1297
Aluriak <lucas.bourneuf@laposte.net>
2016-10-11 22:27:56 +0200

    pep8
    these tab indentation should be replaced by spaces, according to PEP8.
    Please, do it.

--- server/app.py
+++ server/app.py
@@ -6,12 +6,12 @@ mongo.connect("tododb")
 app = Flask(__name__)
 
 
-@app.route("/todo",methods = ['GET','POST'])
+@app.route("/todo", methods = ['GET', 'POST'])
 def notes():
 	print (request.method)
 	if request.method == 'GET':
 		result = [note.export_data() for note in Note.objects.all()]
-		return jsonify({"success":True, "results": result}) 
+		return jsonify({"success": True, "results": result}) 
 
 	if request.method == 'POST':
 
@@ -22,16 +22,16 @@ def notes():
 			note.import_data(request.json)
 			note.save()
 		except Exception as ex:
-			return jsonify({"success":False, "message":"cannot save note : " + str(ex)})
+			return jsonify({"success": False, "message": "cannot save note : " + str(ex)})
 		
-		return jsonify({"success":True, "results": str(note.id)})
+		return jsonify({"success": True, "results": str(note.id)})
 
 @app.route("/todo/<object_id>", methods=["PUT"])
 def set_checked(object_id):
 	try : 
 		note = Note.objects.get(pk = object_id)
 	except Exception as e : 
-		return jsonify({"success":False, "message":str(e)})
+		return jsonify({"success": False, "message": str(e)})
 
 	# Stupid hack... for testing 
 
@@ -41,10 +41,10 @@ def set_checked(object_id):
 	try:
 		note.save()
 	except Exception as e: 
-		return jsonify({"success":False, "message": str(e)})
+		return jsonify({"success": False, "message": str(e)})
 
 
-	return jsonify({"success":True, "results": {}})
+	return jsonify({"success": True, "results": {}})
 
 
 
@@ -53,7 +53,7 @@ def get_note(object_id):
 	try : 
 		note = Note.objects.get(pk = object_id)
 	except : 
-		return jsonify({"success":False, "message": "cannot find the note"})
+		return jsonify({"success": False, "message": "cannot find the note"})
 
 	return jsonify(note.export_data())
 
@@ -62,10 +62,10 @@ def delete_note(object_id):
 	try : 
 		note = Note.objects.get(pk = object_id)
 	except : 
-		return jsonify({"success":False, "message":"cannot find the note"})
+		return jsonify({"success": False, "message": "cannot find the note"})
 
 	note.delete()
-	return jsonify({"success":"true"})
+	return jsonify({"success": "true"})
 
 
 @app.route("/todo/checked", methods=["DELETE"])
@@ -73,9 +73,9 @@ def delete_checked():
 	try:
 		Note.objects(checked=True).delete()
 	except :
-		return jsonify({"success":False, "message":"cannot delete"})
+		return jsonify({"success": False, "message": "cannot delete"})
 
-	return jsonify({"success":"true"})
+	return jsonify({"success": "true"})
 
 
 
